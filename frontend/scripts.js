e6243fe8b0a085840e1873065f2affe0ecd8b8ae
Goldy <112779392+Goldypatel@users.noreply.github.com>
2024-09-27 22:57:48 +0530

    Update scripts.js

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,6 +1,12 @@
 const registerButton = document.getElementById("register-button"); // Correct method
+const loginButton = document.getElementById("sign-in"); // Correct method 
 
 registerButton.addEventListener("click", function() {
     // Use window.location.href to navigate to another HTML page
     window.location.href = './pages/registerP.html'; // Replace with the correct path
+});
+
+loginButton.addEventListener("click", function() {
+    // Use window.location.href to navigate to another HTML page
+    window.location.href = './pages/login.html'; // Replace with the correct path
 });
