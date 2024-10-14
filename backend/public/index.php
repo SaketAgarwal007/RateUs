4774f2f719efc70637469426216b83aa50b7662f
Rajeev Singh <118631381+RajeevSingh2412@users.noreply.github.com>
2024-10-15 02:00:00 +0530

    item backend update

--- backend/public/index.php
+++ backend/public/index.php
@@ -3,6 +3,7 @@ require_once "../confiq/confiq.php";
 require_once "../session/session.php";
 require_once "../app/controllers/user.php";
 require_once "../app/controllers/organization.php";
+require_once "../app/controllers/items.php";
 
 
 $controller="";
@@ -11,13 +12,17 @@ $action="";
 if(isset($_GET['controller']) && isset($_GET['action'])) {
     $controller=$_GET['controller'];
     $action=$_GET['action'];
-}elseif(isset($_POST['action'])) {
+}
+elseif(isset($_POST['action'])){
     $action=$_POST['action'];
     if($action=='register'||$action=='login'||$action=='logout'){
         $controller='users';  // Route to UsersController for user-related actions
     } 
     elseif ($action=='addOrganization') {
         $controller='organizations';  // Route to OrganizationsController
+    }
+    elseif($action=='addItem'){
+        $controller='items';  // Route to ItemsController
     }
 }
 
@@ -39,7 +44,12 @@ switch($controller) {
             $organizationsController->organizationMethods();
         }
         break;
-
+    case 'items':
+        $itemController=new itemController($mysqli);
+        if($action=='addItem'){
+            $itemController->itemMethods();
+        }
+        break;
     default:
         echo "No route found";
         break;
